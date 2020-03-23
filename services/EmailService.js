36e4b52e83d77bffe8a084dc8cd9fa4a2c736133
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2020-03-23 19:45:33 -0400

    Refactored email function

--- services/EmailService.js
+++ services/EmailService.js
@@ -1,17 +1,18 @@
-async function send(mail) {
+async function send({name, email, message}) {
   const sgMail = require("@sendgrid/mail");
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const mailFrom = process.env.MAIL_FROM || "lourework@gmail.com";
+  const subject = process.env.subject || `Novo contato de ${name}`;
   const msg = {
-    to: mail.to,
+    to: email,
     from: mailFrom,
-    subject: mail.subject,
-    text: mail.message,
+    subject,
+    text: message,
     html: `
       <div style="width: 600px; margin: 0 auto;">
           <p>From: ${mailFrom}</p>
-          <h1 style="text-align: center">${mail.subject}</h1>
-          <p>${mail.message}</p>
+          <h1 style="text-align: center">${subject}</h1>
+          <p>${message}</p>
       </div>
   `
   };
