08722e2e7c07b8b708766b30aaeb208252e8cd0d
lourework <bacarolina.g@gmail.com>
2020-03-27 14:56:06 -0400

    Defined correct email

--- services/EmailService.js
+++ services/EmailService.js
@@ -4,13 +4,13 @@ async function send({name, email, message}) {
   const mailFrom = process.env.MAIL_FROM || "lourework@gmail.com";
   const subject = process.env.subject || `Novo contato de ${name}`;
   const msg = {
-    to: email,
+    to: process.env.MAIL_TO || 'lourework@gmail.com',
     from: mailFrom,
     subject,
     text: message,
     html: `
       <div style="width: 600px; margin: 0 auto;">
-          <p>From: ${mailFrom}</p>
+          <p>From: ${email}</p>
           <h1 style="text-align: center">${subject}</h1>
           <p>${message}</p>
       </div>
